90db519aeb9f600dd37b800c360adc2992390e0c
skandatechco <skandatech.co@gmail.com>
2025-10-28 11:07:37 +0530

    Add DocMeta type definition in types.ts

---
+++ lib/types.ts
@@ -0,0 +1,10 @@
+export type DocMeta = {
+  title: string;
+  summary?: string;
+  date?: string;
+  tags?: string[];
+  reviewer?: string;
+  researchable?: boolean;
+  slug: string;
+  lang: string;
+};
