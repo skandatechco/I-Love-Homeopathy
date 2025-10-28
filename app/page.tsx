7cfbb10d35dd7280805ffe5465c91fc251dfec62
skandatechco <skandatech.co@gmail.com>
2025-10-28 15:57:29 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
-// default root "/" if someone goes without /en
 import HomePage from "./[lang]/page";
 
 export default function RootAliasPage() {
+  // default route "/" should show English content
   return <HomePage params={{ lang: "en" }} />;
 }
